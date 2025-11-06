2c01323a0007361b47f8a36899dca582be057598
ryker <testingisoptional@gmail.com>
2025-11-06 14:31:14 -0700

    Drive train

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoThatMoves.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+public class AutoThatMoves extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeftDrive = null;
+    private DcMotor backLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    private DcMotor backRightDrive = null;
+
+    @Override
+    public void runOpMode() {
+
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "motor0");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "motor1");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "motor2");
+        backRightDrive = hardwareMap.get(DcMotor.class, "motor3");
+
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        double power = 1;
+
+        while (opModeIsActive()) {
+
+
+            frontLeftDrive.setPower(power);
+            frontRightDrive.setPower(power);
+            backLeftDrive.setPower(power);
+            backRightDrive.setPower(power);
+
+            if (runtime.seconds() == 10) {
+                power = 0;
+            }
+
+
+
+
+
+
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}
